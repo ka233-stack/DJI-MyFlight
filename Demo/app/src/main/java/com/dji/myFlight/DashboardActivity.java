acb5b1853f2eb8b89cd51e9a254f95f2f15387e4
ka233-stack <1183493652@qq.com>
2021-07-25 20:31:02 +0800

    update dashboard

--- Demo/app/src/main/java/com/dji/myFlight/DashboardActivity.java
+++ Demo/app/src/main/java/com/dji/myFlight/DashboardActivity.java
@@ -3,6 +3,9 @@ package com.dji.myFlight;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DashboardActivity extends AppCompatActivity {
 
