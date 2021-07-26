6d1153d2e3941f08fb67e03ba9e73ac8736967f3
ka233-stack <1183493652@qq.com>
2021-07-26 10:50:36 +0800

    添加图库入口按钮

--- Demo/app/src/main/java/com/dji/myFlight/DashboardActivity.java
+++ Demo/app/src/main/java/com/dji/myFlight/DashboardActivity.java
@@ -3,15 +3,59 @@ package com.dji.myFlight;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class DashboardActivity extends AppCompatActivity {
+import dji.sdk.sdkmanager.DJISDKManager;
+
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView tvConnectionStatus;
+    private TextView tvProductInfo;
+    private ImageButton imgBtnControlManual;
+    private ImageButton imgBtnControlRoutes;
+    private TextView tvSDKVersion;
+    protected ImageButton imgBtnGallery;
+    protected TextView tvLoginState;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        initUI();
+    }
+
+    private void initUI() {
+        tvConnectionStatus = (TextView) findViewById(R.id.text_connection_status);
+        tvProductInfo = (TextView) findViewById(R.id.text_product_info);
+        imgBtnControlManual = (ImageButton) findViewById(R.id.imgBtn_control_manual);
+        imgBtnControlRoutes = (ImageButton) findViewById(R.id.imgBtn_control_routes);
+        imgBtnControlManual.setOnClickListener(this);
+        imgBtnControlRoutes.setOnClickListener(this);
+        tvSDKVersion = (TextView) findViewById(R.id.text_sdk_version);
+        tvSDKVersion.setText(getResources().getString(R.string.sdk_version, DJISDKManager.getInstance().getSDKVersion()));
+        tvLoginState = (TextView) findViewById(R.id.login_state_info);
+        imgBtnGallery = (ImageButton) findViewById(R.id.imgBtn_gallery);
+        imgBtnGallery.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.imgBtn_control_manual) {
+            showToast("手动飞行");
+        } else if (id == R.id.imgBtn_control_routes) {
+            showToast("航线飞行");
+        } else if(id == R.id.imgBtn_gallery){
+            showToast("图库");
+        }
+    }
+
+    private void showToast(final String toastMsg) {
+        runOnUiThread(() -> Toast.makeText(getApplicationContext(), toastMsg, Toast.LENGTH_LONG).show());
     }
 }
