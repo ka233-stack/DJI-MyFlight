7d1732df75c09cbd2362a9101d1186f97b17a405
ka233-stack <1183493652@qq.com>
2021-07-28 19:15:53 +0800

    Update ManualFlightActivity.java

--- Demo/app/src/main/java/com/dji/myFlight/ManualFlightActivity.java
+++ Demo/app/src/main/java/com/dji/myFlight/ManualFlightActivity.java
@@ -1,11 +1,13 @@
 package com.dji.myFlight;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -34,310 +36,23 @@ import dji.ux.beta.core.widget.useraccount.UserAccountLoginWidget;
 import dji.ux.beta.map.widget.map.MapWidget;
 import dji.ux.beta.training.widget.simulatorcontrol.SimulatorControlWidget;
 
-public class ManualFlightActivity extends AppCompatActivity {
+public class ManualFlightActivity extends AppCompatActivity implements View.OnClickListener {
 
-    //region Fields
-    private final static String TAG = "DefaultLayoutActivity";
+    private Button mMediaManagerBtn;
 
-    @BindView(R.id.widget_radar)
-    protected RadarWidget radarWidget;
-    @BindView(R.id.widget_fpv)
-    protected FPVWidget fpvWidget;
-    @BindView(R.id.widget_fpv_interaction)
-    protected FPVInteractionWidget fpvInteractionWidget;
-    @BindView(R.id.widget_map)
-    protected MapWidget mapWidget;
-    @BindView(R.id.widget_secondary_fpv)
-    protected FPVWidget secondaryFPVWidget;
-    @BindView(R.id.root_view)
-    protected ConstraintLayout parentView;
-    @BindView(R.id.widget_panel_system_status_list)
-    protected SystemStatusListPanelWidget systemStatusListPanelWidget;
-
-    @BindView(R.id.widget_rtk)
-    protected RTKWidget rtkWidget;
-    @BindView(R.id.widget_simulator_control)
-    protected SimulatorControlWidget simulatorControlWidget;
-
-    private boolean isMapMini = true;
-    private int widgetHeight;
-    private int widgetWidth;
-    private int widgetMargin;
-    private int deviceWidth;
-    private int deviceHeight;
-    private CompositeDisposable compositeDisposable;
-    private UserAccountLoginWidget userAccountLoginWidget;
-    //endregion
-
-    //region Lifecycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_beta_manual_flight);
-
-        widgetHeight = (int) getResources().getDimension(R.dimen.mini_map_height);
-        widgetWidth = (int) getResources().getDimension(R.dimen.mini_map_width);
-        widgetMargin = (int) getResources().getDimension(R.dimen.mini_map_margin);
-
-        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-        deviceHeight = displayMetrics.heightPixels;
-        deviceWidth = displayMetrics.widthPixels;
-
-        ButterKnife.bind(this);
-        setM200SeriesWarningLevelRanges();
-        mapWidget.initAMap(map -> {
-            map.setOnMapClickListener(latLng -> onViewClick(mapWidget));
-            map.getUiSettings().setZoomControlsEnabled(false);
-        });
-        mapWidget.getUserAccountLoginWidget().setVisibility(View.GONE);
-        mapWidget.onCreate(savedInstanceState);
-
-        // Setup top bar state callbacks
-        TopBarPanelWidget topBarPanel = findViewById(R.id.panel_top_bar);
-        SystemStatusWidget systemStatusWidget = topBarPanel.getSystemStatusWidget();
-        if (systemStatusWidget != null) {
-            systemStatusWidget.setStateChangeCallback(findViewById(R.id.widget_panel_system_status_list));
-        }
-
-        SimulatorIndicatorWidget simulatorIndicatorWidget = topBarPanel.getSimulatorIndicatorWidget();
-        if (simulatorIndicatorWidget != null) {
-            simulatorIndicatorWidget.setStateChangeCallback(findViewById(R.id.widget_simulator_control));
-        }
-
-        GPSSignalWidget gpsSignalWidget = topBarPanel.getGPSSignalWidget();
-        if (gpsSignalWidget != null) {
-            gpsSignalWidget.setStateChangeCallback(findViewById(R.id.widget_rtk));
-        }
-
-        userAccountLoginWidget = mapWidget.getUserAccountLoginWidget();
-        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) userAccountLoginWidget.getLayoutParams();
-        params.topMargin = (deviceHeight / 10) + (int) DisplayUtil.dipToPx(this, 10);
-        userAccountLoginWidget.setLayoutParams(params);
+        setContentView(R.layout.activity_manual_flight);
+        mMediaManagerBtn = (Button) findViewById(R.id.btn_mediaManager);
+        mMediaManagerBtn.setOnClickListener(this);
     }
 
     @Override
-    protected void onDestroy() {
-        mapWidget.onDestroy();
-        super.onDestroy();
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-        mapWidget.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapWidget.onLowMemory();
-
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        mapWidget.onResume();
-        compositeDisposable = new CompositeDisposable();
-        compositeDisposable.add(secondaryFPVWidget.getCameraName()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(this::updateSecondaryVideoVisibility));
-
-        compositeDisposable.add(systemStatusListPanelWidget.closeButtonPressed()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(pressed -> {
-                    if (pressed) {
-                        ViewExtensions.hide(systemStatusListPanelWidget);
-                    }
-                }));
-
-        compositeDisposable.add(rtkWidget.getUIStateUpdates()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(uiState -> {
-                    if (uiState instanceof RTKWidget.UIState.VisibilityUpdated) {
-                        if (((RTKWidget.UIState.VisibilityUpdated) uiState).isVisible()) {
-                            hideOtherPanels(rtkWidget);
-                        }
-                    }
-                }));
-        compositeDisposable.add(simulatorControlWidget.getUIStateUpdates()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(simulatorControlWidgetState -> {
-                    if (simulatorControlWidgetState instanceof SimulatorControlWidget.UIState.VisibilityUpdated) {
-                        if (((SimulatorControlWidget.UIState.VisibilityUpdated) simulatorControlWidgetState).isVisible()) {
-                            hideOtherPanels(simulatorControlWidget);
-                        }
-                    }
-                }));
-    }
-
-    @Override
-    protected void onPause() {
-        if (compositeDisposable != null) {
-            compositeDisposable.dispose();
-            compositeDisposable = null;
-        }
-        mapWidget.onPause();
-        super.onPause();
-    }
-    //endregion
-
-    //region Utils
-
-    private void hideOtherPanels(@Nullable View widget) {
-        View[] panels = {
-                rtkWidget,
-                simulatorControlWidget
-        };
-
-        for (View panel : panels) {
-            if (widget != panel) {
-                panel.setVisibility(View.GONE);
-            }
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_mediaManager) {
+            Intent intent = new Intent(this, GalleryActivity.class);
+            startActivity(intent);
         }
     }
-
-    private void setM200SeriesWarningLevelRanges() {
-        Model[] m200SeriesModels = {
-                Model.MATRICE_200,
-                Model.MATRICE_210,
-                Model.MATRICE_210_RTK,
-                Model.MATRICE_200_V2,
-                Model.MATRICE_210_V2,
-                Model.MATRICE_210_RTK_V2
-        };
-        float[] ranges = {70, 30, 20, 12, 6, 3};
-        radarWidget.setWarningLevelRanges(m200SeriesModels, ranges);
-    }
-
-    /**
-     * Handles a click event on the FPV widget
-     */
-    @OnClick(R.id.widget_fpv)
-    public void onFPVClick() {
-        onViewClick(fpvWidget);
-    }
-
-    /**
-     * Handles a click event on the secondary FPV widget
-     */
-    @OnClick(R.id.widget_secondary_fpv)
-    public void onSecondaryFPVClick() {
-        swapVideoSource();
-    }
-
-    /**
-     * Swaps the FPV and Map Widgets.
-     *
-     * @param view The thumbnail view that was clicked.
-     */
-    private void onViewClick(View view) {
-        if (view == fpvWidget && !isMapMini) {
-            //reorder widgets
-            parentView.removeView(fpvWidget);
-            parentView.addView(fpvWidget, 0);
-
-            //resize widgets
-            resizeViews(fpvWidget, mapWidget);
-            //enable interaction on FPV
-            fpvInteractionWidget.setInteractionEnabled(true);
-            //disable user login widget on map
-            userAccountLoginWidget.setVisibility(View.GONE);
-            isMapMini = true;
-        } else if (view == mapWidget && isMapMini) {
-            //reorder widgets
-            parentView.removeView(fpvWidget);
-            parentView.addView(fpvWidget, parentView.indexOfChild(mapWidget) + 1);
-            //resize widgets
-            resizeViews(mapWidget, fpvWidget);
-            //disable interaction on FPV
-            fpvInteractionWidget.setInteractionEnabled(false);
-            //enable user login widget on map
-            userAccountLoginWidget.setVisibility(View.VISIBLE);
-            isMapMini = false;
-        }
-    }
-
-    /**
-     * Helper method to resize the FPV and Map Widgets.
-     *
-     * @param viewToEnlarge The view that needs to be enlarged to full screen.
-     * @param viewToShrink  The view that needs to be shrunk to a thumbnail.
-     */
-    private void resizeViews(View viewToEnlarge, View viewToShrink) {
-        //enlarge first widget
-        ResizeAnimation enlargeAnimation = new ResizeAnimation(viewToEnlarge, widgetWidth, widgetHeight, deviceWidth, deviceHeight, 0);
-        viewToEnlarge.startAnimation(enlargeAnimation);
-
-        //shrink second widget
-        ResizeAnimation shrinkAnimation = new ResizeAnimation(viewToShrink, deviceWidth, deviceHeight, widgetWidth, widgetHeight, widgetMargin);
-        viewToShrink.startAnimation(shrinkAnimation);
-    }
-
-    /**
-     * Swap the video sources of the FPV and secondary FPV widgets.
-     */
-    private void swapVideoSource() {
-        if (secondaryFPVWidget.getVideoSource() == SettingDefinitions.VideoSource.SECONDARY) {
-            fpvWidget.setVideoSource(SettingDefinitions.VideoSource.SECONDARY);
-            secondaryFPVWidget.setVideoSource(SettingDefinitions.VideoSource.PRIMARY);
-        } else {
-            fpvWidget.setVideoSource(SettingDefinitions.VideoSource.PRIMARY);
-            secondaryFPVWidget.setVideoSource(SettingDefinitions.VideoSource.SECONDARY);
-        }
-    }
-
-    /**
-     * Hide the secondary FPV widget when there is no secondary camera.
-     *
-     * @param cameraName The name of the secondary camera.
-     */
-    private void updateSecondaryVideoVisibility(String cameraName) {
-        if (cameraName.equals(PhysicalSource.UNKNOWN.name())) {
-            secondaryFPVWidget.setVisibility(View.GONE);
-        } else {
-            secondaryFPVWidget.setVisibility(View.VISIBLE);
-        }
-    }
-    //endregion
-
-    //region classes
-
-    /**
-     * Animation to change the size of a view.
-     */
-    private static class ResizeAnimation extends Animation {
-
-        private static final int DURATION = 300;
-
-        private View view;
-        private int toHeight;
-        private int fromHeight;
-        private int toWidth;
-        private int fromWidth;
-        private int margin;
-
-        private ResizeAnimation(View v, int fromWidth, int fromHeight, int toWidth, int toHeight, int margin) {
-            this.toHeight = toHeight;
-            this.toWidth = toWidth;
-            this.fromHeight = fromHeight;
-            this.fromWidth = fromWidth;
-            view = v;
-            this.margin = margin;
-            setDuration(DURATION);
-        }
-
-        @Override
-        protected void applyTransformation(float interpolatedTime, Transformation t) {
-            float height = (toHeight - fromHeight) * interpolatedTime + fromHeight;
-            float width = (toWidth - fromWidth) * interpolatedTime + fromWidth;
-            ConstraintLayout.LayoutParams p = (ConstraintLayout.LayoutParams) view.getLayoutParams();
-            p.height = (int) height;
-            p.width = (int) width;
-            p.rightMargin = margin;
-            p.bottomMargin = margin;
-            view.requestLayout();
-        }
-    }
-    //endregion
 }
