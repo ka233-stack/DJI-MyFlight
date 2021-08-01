18d3e079995094d313fb09554327278ba93c39d6
ka233-stack <1183493652@qq.com>
2021-08-01 09:53:05 +0800

    fixed type error

--- Demo/beta-sample/src/main/java/com/dji/ux/beta/sample/Waypoint1Activity.java
+++ Demo/beta-sample/src/main/java/com/dji/ux/beta/sample/Waypoint1Activity.java
@@ -109,8 +109,8 @@ public class Waypoint1Activity extends FragmentActivity implements View.OnClickL
     protected ScrollView point_settings_scroll_view;
     protected ImageView btnPanel;
 
-    private boolean selectedWaypoint=false;
-    protected EditText change_point_v,change_point_v1;
+    private boolean selectedWaypoint = false;
+    protected EditText change_point_v, change_point_v1;
     protected Button btn_change_commit;
     private Marker changeMarker;
     protected ImageView btn_chang_settings;
@@ -175,7 +175,7 @@ public class Waypoint1Activity extends FragmentActivity implements View.OnClickL
         change_point_v = (EditText) findViewById(R.id.change_point_v);
         change_point_v1 = (EditText) findViewById(R.id.change_point_v1);
         btn_change_commit = (Button) findViewById(R.id.btn_change_commit);
-        btn_chang_settings = (ImageView) findViewById(R.id.btn_chang_settings);
+        btn_chang_settings = (ImageView) findViewById(R.id.btn_change_settings);
         btn_change_commit.setOnClickListener(this);
         changePoint_text = (TextView) findViewById(R.id.changePoint_text);
         point_settings_scroll_view = (ScrollView) findViewById(R.id.point_settings_scroll_view);
@@ -301,20 +301,6 @@ public class Waypoint1Activity extends FragmentActivity implements View.OnClickL
         initUI();
         addListener();
         initFlightController();
-
-        btnPanel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                movePanel();
-            }
-        });
-
-        btn_chang_settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                movePanel_copy();
-            }
-        });
     }
 
     private PolylineOptions getFinishedPolylineOptions() {
@@ -570,8 +556,8 @@ public class Waypoint1Activity extends FragmentActivity implements View.OnClickL
             updateDroneLocation();
             lastPoint = null;
 
-            selectedWaypoint=false;
-            changeMarker=null;
+            selectedWaypoint = false;
+            changeMarker = null;
             showLatLng(changeMarker);
         } else if (id == R.id.btn_upload) {
             set_settings();
@@ -587,8 +573,12 @@ public class Waypoint1Activity extends FragmentActivity implements View.OnClickL
             stopWaypointMission();
         } else if (id == R.id.btn_commit) {
             addPointByLonLat();
-        } else if (id == R.id.btn_change_commit){
+        } else if (id == R.id.btn_change_commit) {
             changeCommit(changeMarker);
+        } else if (id == R.id.btn_settings) {
+            movePanel();
+        }else if (id == R.id.btn_change_settings) {
+            movePanel_copy();
         }
     }
 
@@ -931,8 +921,8 @@ public class Waypoint1Activity extends FragmentActivity implements View.OnClickL
         point_settings_scroll_view.startAnimation(animate);
     }
 
-    private void showLatLng(Marker mark){
-        if(selectedWaypoint) {
+    private void showLatLng(Marker mark) {
+        if (selectedWaypoint) {
             LatLng position = mark.getPosition();
             int index;
             index = -1;
@@ -940,25 +930,23 @@ public class Waypoint1Activity extends FragmentActivity implements View.OnClickL
                 if (mapEntry.getValue().equals(mark))
                     index = mapEntry.getKey();
             }
-            changePoint_text.setText("更改某点坐标(点"+String.valueOf(index+1)+")");
+            changePoint_text.setText("更改某点坐标(点" + String.valueOf(index + 1) + ")");
             change_point_v.setText(String.valueOf(position.latitude));
             change_point_v1.setText(String.valueOf(position.longitude));
-        }
-        else{
+        } else {
             changePoint_text.setText("更改某点坐标");
             change_point_v.setText("请先选点");
             change_point_v1.setText("请先选点");
         }
     }
 
-    private void changeCommit(Marker marker){
-        if(!selectedWaypoint){
+    private void changeCommit(Marker marker) {
+        if (!selectedWaypoint) {
             showToast("请先选中一个点");
-        }
-        else{
+        } else {
             double change_v = Double.parseDouble(change_point_v.getText().toString());
             double change_v1 = Double.parseDouble(change_point_v1.getText().toString());
-            LatLng position = new LatLng(change_v,change_v1);
+            LatLng position = new LatLng(change_v, change_v1);
             marker.setPosition(position);
 
             int index, lastIndex;
